94c189af1fd2fe6bc80adb88933e84b56a31861f
TMCooper# <pokemon160377@gmail.com>
2024-10-17 16:30:13 +0200

    Update Voiranime_Downloader.py

--- Voiranime_Downloader.py
+++ Voiranime_Downloader.py
@@ -1,9 +1,12 @@
+# installer un proxy via Yui pour eviter que le captcha repères les activité de haute fréquences et facilité la resolution de celle ci
+
 from function.__init__ import *
-import requests
 
 def main():
     original_url = "https://v5.voiranime.com/anime/komi-san-wa-komyushou-desu/komi-san-wa-komyushou-desu-01-vostfr/"
-    Yui.requesting_anime(original_url)
+
+    # Yui.requesting_anime(original_url)
+    Thoru.captcha(original_url)
 
 if __name__ == "__main__":
     main()
